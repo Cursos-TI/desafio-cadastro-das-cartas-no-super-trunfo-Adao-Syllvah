4733390d2ee29525207b8b72e129280e004dd12d
Adao-Syllvah <adaomyster@gmail.com>
2025-03-16 23:23:16 +0000

    CartasSuperTrunfo versão 1.2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,6 +8,7 @@ int main() {
     int populacao, numerosPontosTuristicos, populacao2, numerosPontosTuristicos2;
     float area, pib, area2, pib2;
     
+    
     //Entrada de dados primeira carta
     printf("Carta 1: \n");
     printf("Digite a letra do primeiro estado de A a H: ");
@@ -24,7 +25,8 @@ int main() {
     scanf("%f", &pib);
     printf("Numeros de pontos turisticos: ");
     scanf("%d", &numerosPontosTuristicos);
-
+    
+    
     
     //Entrada de dados segunda carta
     printf("\nCarta 2:\n");
@@ -42,10 +44,17 @@ int main() {
     scanf("%f", &pib2);
     printf("Numeros de pontos turisticos: ");
     scanf("%d", &numerosPontosTuristicos2);
-    
+
+
+    //Cálculo Densidade populacional e PIB PerCapita
+    double densidadePopulacional = (double) populacao / area;
+    double densidadePopulacional2 = (double) populacao2 / area2;
+    double pibPerCapita = (double) pib / populacao;
+    double pibPerCapita2 = (double) pib2 / populacao2;
+
     //Exibição de dados digitados
-    printf("\nCarta 1 \n Estado: %c\n Codigo: %c%s\n Nome da cidade: %s\n Populacao: %d\n area: %f Km2\n PIB: %f\n Numero de pontos turisticos: %d \n\n", estado, estado, codigoCarta, nomeCidade, populacao, area, pib, numerosPontosTuristicos);
-    printf("Carta 2 \n Estado: %c\n Codigo: %c%s\n Nome da cidade: %s\n Populacao: %d\n area: %f Km2\n PIB: %f\n Numero de pontos turisticos: %d \n\n", estado2, estado2, codigoCarta2, nomeCidade2, populacao2, area2, pib2, numerosPontosTuristicos2);
+    printf("\nCarta 1 \n Estado: %c\n Codigo: %c%s\n Nome da cidade: %s\n Populacao: %d\n area: %.2f Km2\n PIB: %.2f\n Numero de pontos turisticos: %d \n Densidade Populacional: %.2f Hab/Km2\n PIB per Capita: %.2f reais\n\n", estado, estado, codigoCarta, nomeCidade, populacao, area, pib, numerosPontosTuristicos, densidadePopulacional, pibPerCapita);
+    printf("Carta 2 \n Estado: %c\n Codigo: %c%s\n Nome da cidade: %s\n Populacao: %d\n area: %.2f Km2\n PIB: %.2f\n Numero de pontos turisticos: %d \n Densidade Populacional: %.2f Hab/Km2\n PIB per Capita: %.2f reais\n\n", estado2, estado2, codigoCarta2, nomeCidade2, populacao2, area2, pib2, numerosPontosTuristicos2, densidadePopulacional2, pibPerCapita2);
     
 
     return 0;
